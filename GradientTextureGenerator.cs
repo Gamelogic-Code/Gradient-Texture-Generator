2320e2ba53eb01c911576f2f33db3a4f57d99500
hermantulleken <herman.tulleken@gmail.com>
2025-01-29 17:39:48 -0300

    Added additional patterns and improved gradient texture generator.

--- GradientTextureGenerator.cs
+++ GradientTextureGenerator.cs
@@ -1,11 +1,12 @@
 // Author: Herman Tulleken
-// gamelogic.co.za
+// www.gamelogic.co.za
 
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using Random = UnityEngine.Random;
 
 namespace Gamelogic.Experimental.Tools.Editor
 {
@@ -107,7 +108,7 @@ namespace Gamelogic.Experimental.Tools.Editor
 	/// <summary>
 	/// Editor window for generating gradient textures.
 	/// </summary>
-	public class GradientTextureGenerator : EditorWindow
+	public class RampTextureGenerator : EditorWindow
 	{
 		private enum Direction
 		{
@@ -119,6 +120,7 @@ namespace Gamelogic.Experimental.Tools.Editor
 
 		private enum GradientType
 		{
+			SingleColor,
 			Gradient,
 			Curve
 		}
@@ -142,15 +144,43 @@ namespace Gamelogic.Experimental.Tools.Editor
 			}
 		};
 
-		private AnimationCurve colorCurve = new();
-		private AnimationCurve alphaCurve = new();
+		private AnimationCurve colorCurve = new()
+		{
+			keys = new[]
+			{
+				new Keyframe(0, 0)
+				{
+					outTangent = 1,
+				},
+				new Keyframe(1, 1)
+				{
+					inTangent = 1
+				}
+			},
+			
+			postWrapMode = WrapMode.Clamp,
+			preWrapMode = WrapMode.Clamp,
+		};
+
+		private AnimationCurve alphaCurve = new()
+		{
+			keys = new[]
+			{
+				new Keyframe(0, 1),
+				new Keyframe(1, 1)
+			},
+
+			postWrapMode = WrapMode.Clamp,
+			preWrapMode = WrapMode.Clamp,
+		};
 		
+		private Color singleColor = Color.white;
 		private ColorList colorList;
 		private SerializedObject serializedObject;
 		private SerializedProperty colorsProperty;
 		private Vector2Int imageDimensions = new(DefaultImageSize, DefaultImageSize);
 
-		private GradientType gradientType = GradientType.Gradient;
+		private GradientType gradientType = GradientType.Curve;
 		private Direction direction = Direction.X;
 
 		private bool discreteSteps = false;
@@ -160,10 +190,9 @@ namespace Gamelogic.Experimental.Tools.Editor
 		private bool circularGradient;
 
 		private Texture2D previewTexture;
-
-
+		
 		[MenuItem(ToolMenuPath)]
-		public static void ShowWindow() => GetWindow<GradientTextureGenerator>(ToolName);
+		public static void ShowWindow() => GetWindow<RampTextureGenerator>(ToolName);
 
 		private void OnEnable()
 		{
@@ -187,6 +216,7 @@ namespace Gamelogic.Experimental.Tools.Editor
 			EditorGUILayout.Space();
 			
 			Header("Sample Settings");
+			DrawSingleColorControls();
 			DrawGradientControls();
 			DrawCurveControls();
 			DrawDiscreteStepControls();
@@ -215,6 +245,16 @@ namespace Gamelogic.Experimental.Tools.Editor
 
 			return;
 			
+			void DrawSingleColorControls()
+			{
+				if (gradientType != GradientType.SingleColor)
+				{
+					return;
+				}
+				
+				singleColor = EditorGUILayout.ColorField(singleColor);
+			}
+			
 			void DrawGradientControls()
 			{
 				if (gradientType != GradientType.Gradient)
@@ -239,6 +279,7 @@ namespace Gamelogic.Experimental.Tools.Editor
 
 			void DrawDiscreteStepControls()
 			{
+				EditorGUI.BeginDisabledGroup(gradientType == GradientType.SingleColor);
 				discreteSteps = EditorGUILayout.Toggle("Discrete Steps", discreteSteps);
 				EditorGUI.BeginDisabledGroup(!discreteSteps);
 				steps = EditorGUILayout.IntField("Steps", steps);
@@ -247,15 +288,35 @@ namespace Gamelogic.Experimental.Tools.Editor
 				circularGradient = EditorGUILayout.Toggle("Circular gradient", circularGradient);
 				
 				EditorGUI.EndDisabledGroup();
+				EditorGUI.EndDisabledGroup();
 			}
 
-			void DrawImageSizeControls() 
-				=> imageDimensions = EditorGUILayout.Vector2IntField("Image Dimensions", imageDimensions);
+			void DrawImageSizeControls()
+			{
+				EditorGUILayout.BeginHorizontal();
+				if(GUILayout.Button("512x512"))
+				{
+					imageDimensions = new Vector2Int(512, 512);
+				}
+				if(GUILayout.Button("256x256"))
+				{
+					imageDimensions = new Vector2Int(256, 256);
+				}
+				if(GUILayout.Button("256x8"))
+				{
+					imageDimensions = new Vector2Int(256, 8);
+				}
+				
+				EditorGUILayout.EndHorizontal();
+				imageDimensions = EditorGUILayout.Vector2IntField("Image Dimensions", imageDimensions);
+			}
 
 			void DrawDirectionControls()
 			{
+				EditorGUI.BeginDisabledGroup(gradientType == GradientType.SingleColor);
 				direction = (Direction)EditorGUILayout.EnumPopup("Direction", direction);
 				flipT = EditorGUILayout.Toggle("Flip T", flipT);
+				EditorGUI.EndDisabledGroup();
 			}
 			
 			void DrawAngularControls()
@@ -264,8 +325,9 @@ namespace Gamelogic.Experimental.Tools.Editor
 				{
 					return;
 				}
-				
+				EditorGUI.BeginDisabledGroup(gradientType == GradientType.SingleColor);
 				offsetAngle = EditorGUILayout.FloatField("Offset Angle (Degrees)", offsetAngle);
+				EditorGUI.EndDisabledGroup();
 			}
 
 			void DrawPreviewTexture()
@@ -398,10 +460,217 @@ namespace Gamelogic.Experimental.Tools.Editor
 		private Color GetColor(float t) 
 			=> gradientType switch
 			{
+				GradientType.SingleColor => singleColor,
 				GradientType.Gradient => gradient.Evaluate(t),
 				GradientType.Curve => colorList.Evaluate(colorCurve.Evaluate(t)),
 				_ => throw new ArgumentOutOfRangeException($"No implementation for {gradientType} of type {gradientType.GetType()}")
 			};
+
+		private void OnDestroy()
+		{
+			if (previewTexture != null)
+			{
+				DestroyImmediate(previewTexture);
+			}
+		}
+	}
+	
+	public enum PatternType
+	{
+		WhiteNoise,
+		CheckerBoard,
+		HslColor
+	}
+	
+	/// <summary>
+	/// Editor window for generating checkerboard textures.
+	/// </summary>
+	public class CheckerboardTextureGenerator : EditorWindow
+	{
+		private const string ToolName = "Pattern Texture Generator";
+		private const string ToolMenuPath = "Gamelogic/Tools/" + ToolName;
+		private const int DefaultImageSize = 256;
+		
+		private PatternType patternType = PatternType.CheckerBoard;
+		private Vector2Int imageDimensions = new(DefaultImageSize, DefaultImageSize);
+		private Vector2Int cellDimensions = new(32, 32);
+		private Color color1 = Color.black;
+		private Color color2 = Color.white;
+		private Texture2D previewTexture;
+
+		[MenuItem(ToolMenuPath)]
+		public static void ShowWindow() => GetWindow<CheckerboardTextureGenerator>(ToolName);
+
+		private void OnEnable()
+		{
+			GeneratePreviewTexture();
+		}
+
+		private void OnGUI()
+		{
+			EditorGUI.BeginChangeCheck();
+			
+			Header("Colors");
+			color1 = EditorGUILayout.ColorField("Color 1", color1);
+			color2 = EditorGUILayout.ColorField("Color 2", color2);
+			
+			patternType = (PatternType)EditorGUILayout.EnumPopup("Pattern Type", patternType);
+			
+			Header("Pattern Settings");
+			switch (patternType)
+			{
+				case PatternType.CheckerBoard:
+					cellDimensions = EditorGUILayout.Vector2IntField("Cell Dimensions", cellDimensions);
+					break;
+				
+			}
+			
+			Header("Output Settings");
+			
+			imageDimensions = EditorGUILayout.Vector2IntField("Image Dimensions", imageDimensions);
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				GeneratePreviewTexture();
+			}
+
+			DrawPreviewTexture();
+
+			if (GUILayout.Button("Save Texture"))
+			{
+				GenerateAndSaveTexture();
+			}
+		}
+
+		private void Header(string text) => EditorGUILayout.LabelField(text, EditorStyles.boldLabel);
+
+		private void GeneratePreviewTexture()
+		{
+			if (previewTexture != null)
+			{
+				DestroyImmediate(previewTexture);
+			}
+
+			previewTexture = GenerateTexture(imageDimensions);
+			Repaint();
+		}
+
+		private Texture2D GenerateTexture(Vector2Int dimensions)
+		{
+			switch (patternType)
+			{
+				case PatternType.CheckerBoard:
+					return GenerateTexture_CheckerBoard(dimensions);
+				case PatternType.WhiteNoise:
+					return GenerateTexture_WhiteNoise(dimensions);
+				case PatternType.HslColor:
+					return GenerateTexture_HslColor(dimensions);
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+			
+		}
+
+		private void DrawPreviewTexture()
+		{
+			if (previewTexture == null)
+			{
+				return;
+			}
+
+			EditorGUILayout.Space();
+			Header("Preview");
+			var previewRect = GUILayoutUtility.GetRect(256, 256, GUILayout.ExpandWidth(true));
+			EditorGUI.DrawPreviewTexture(previewRect, previewTexture, null, ScaleMode.ScaleToFit);
+		}
+
+		private void GenerateAndSaveTexture()
+		{
+			string path = EditorUtility.SaveFilePanel("Save Texture", string.Empty, "CheckerboardTexture.png", "png");
+
+			if (string.IsNullOrEmpty(path))
+			{
+				return;
+			}
+
+			var texture = GenerateTexture(imageDimensions);
+			texture.wrapMode = TextureWrapMode.Clamp;
+
+			byte[] bytes = texture.EncodeToPNG();
+			File.WriteAllBytes(path, bytes);
+			AssetDatabase.Refresh();
+
+			DestroyImmediate(texture);
+
+			EditorUtility.DisplayDialog("Success", "Texture saved to:\n" + path, "OK");
+		}
+
+		private Texture2D GenerateTexture_CheckerBoard(Vector2Int dimensions)
+		{
+			var texture = new Texture2D(dimensions.x, dimensions.y, TextureFormat.RGBA32, false);
+
+			for (int x = 0; x < dimensions.x; x++)
+			{
+				for (int y = 0; y < dimensions.y; y++)
+				{
+					bool isColor1 = ((x / cellDimensions.x) + (y / cellDimensions.y)) % 2 == 0;
+					texture.SetPixel(x, y, isColor1 ? color1 : color2);
+				}
+			}
+
+			texture.wrapMode = TextureWrapMode.Clamp;
+			texture.Apply();
+			return texture;
+		}
+		
+		private Texture2D GenerateTexture_WhiteNoise(Vector2Int dimensions)
+		{
+			var texture = new Texture2D(dimensions.x, dimensions.y, TextureFormat.RGBA32, false);
+
+			for (int x = 0; x < dimensions.x; x++)
+			{
+				for (int y = 0; y < dimensions.y; y++)
+				{
+					float randomValue = Random.value; // Random value between 0 and 1
+					Color randomColor = Color.Lerp(color1, color2, randomValue);
+					texture.SetPixel(x, y, randomColor);
+				}
+			}
+
+			texture.wrapMode = TextureWrapMode.Clamp;
+			texture.Apply();
+			return texture;
+		}
+		
+		private Texture2D GenerateTexture_HslColor(Vector2Int dimensions)
+		{
+			var texture = new Texture2D(dimensions.x, dimensions.y, TextureFormat.RGBA32, false);
+
+			for (int x = 0; x < dimensions.x; x++)
+			{
+				for (int y = 0; y < dimensions.y; y++)
+				{
+					float u = x / (dimensions.x - 1f);
+					float v = y / (dimensions.y - 1f);
+					Color color;
+					Color hsl = Color.HSVToRGB(u, 1f, 1f);
+					if (v < 0.5)
+					{
+						color = Color.Lerp(Color.black, hsl, v * 2f);
+					}
+					else
+					{
+						color = Color.Lerp(hsl, Color.white, (v - 0.5f) * 2f);
+					}
+						
+					texture.SetPixel(x, y, color);
+				}
+			}
+
+			texture.wrapMode = TextureWrapMode.Clamp;
+			texture.Apply();
+			return texture;
+		}
 
 		private void OnDestroy()
 		{
